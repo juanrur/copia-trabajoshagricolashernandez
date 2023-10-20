6b49f27f9bcd9026de7a4aa57b3609b0658f14be
Juan Ruiz <juanruizcampa@gmail.com>
2023-10-20 20:35:43 +0200

    firt commit,init screen

---
+++ src/info/funciones-empresa.js
@@ -0,0 +1,20 @@
+export const articles = [
+  {
+  title: "Servicios de nivelación",
+  paragraph: `Ofrecemos un amplio abanico de servicios en esta área, desde la nivelación hasta la plantación de cultivos.
+<br /> <br />
+  Nuestro equipo de profesionales es especialista y cuenta con la mejor formación a fin de ofrecer un trabajo excepcional.
+<br /> <br />
+  Estamos comprometidos con la satisfacción de nuestros clientes, para garantizar la calidad de nuestros trabajos.` ,
+  video: "https://www.youtube.com/embed/aUDoX6kz0Lc?si=XPnOYN6ZMusrmspd"
+  },
+  {
+    title: "Transformación de cultivos",
+    paragraph: `Ayudamos a pequeños, medianos y grandes agricultores en la transformación de sus cultivos.
+<br /> <br />
+    Trabajos Agrícolas Hernández es una empresa de transformación de terrenos con más de 20 años de experiencia en el sector. 
+<br /> <br />    
+    Ofrecermos un servicio integral y diferenciador que incluye la preparación del terreno, la fertilización, la irrigación, la siembra, el control de plagas y enfermedades, y la recolección.`,
+    video: 'https://www.youtube.com/embed/44yCiFPnzew?si=wdMOeoXNkhymx0t6'
+  }
+]
