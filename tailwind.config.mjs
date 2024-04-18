a2ae39a0a93f8c1d048802dad24dc6c763fbfd92
Juan Ruiz <juanruizcampa@gmail.com>
2024-04-18 13:54:11 +0200

    advance in the project and refactoring

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,11 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				bgcolor: "#f1f1f1"
+			}
+		},
 	},
 	plugins: [],
 }
